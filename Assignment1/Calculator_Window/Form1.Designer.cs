c32e74b0963a40606d5d41c434ec3a9b1207f733
WhuerCSsheng <142279505+WhuerCSsheng@users.noreply.github.com>
2025-02-26 23:08:48 +0800

    Add files via upload

---
+++ Assignment1/Calculator_Window/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Calculator_Window
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(335, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 74);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(172, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 74);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(12, 377);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 74);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(335, 297);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 74);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button5.Location = new System.Drawing.Point(172, 297);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(142, 74);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button6.Location = new System.Drawing.Point(12, 297);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(142, 74);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button7.Location = new System.Drawing.Point(335, 217);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(142, 74);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button8.Location = new System.Drawing.Point(172, 217);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(142, 74);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button9.Location = new System.Drawing.Point(12, 217);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(142, 74);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button10.Location = new System.Drawing.Point(172, 137);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(305, 74);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Delete";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button11.Location = new System.Drawing.Point(12, 137);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(142, 74);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "CE";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.MaximumSize = new System.Drawing.Size(190, 50);
+            this.textBox1.MaxLength = 8;
+            this.textBox1.MinimumSize = new System.Drawing.Size(190, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 50);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBox1.Location = new System.Drawing.Point(208, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(69, 48);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(287, 12);
+            this.textBox2.MaximumSize = new System.Drawing.Size(190, 50);
+            this.textBox2.MaxLength = 8;
+            this.textBox2.MinimumSize = new System.Drawing.Size(190, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(190, 50);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button12.Location = new System.Drawing.Point(172, 457);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(142, 74);
+            this.button12.TabIndex = 15;
+            this.button12.Text = "0";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button13.Location = new System.Drawing.Point(335, 457);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(142, 74);
+            this.button13.TabIndex = 16;
+            this.button13.Text = "=";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button14.Location = new System.Drawing.Point(12, 457);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(142, 74);
+            this.button14.TabIndex = 17;
+            this.button14.Text = ".";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("宋体", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox3.Location = new System.Drawing.Point(12, 68);
+            this.textBox3.MaximumSize = new System.Drawing.Size(500, 60);
+            this.textBox3.MaxLength = 20;
+            this.textBox3.MinimumSize = new System.Drawing.Size(450, 60);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(465, 60);
+            this.textBox3.TabIndex = 18;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 527);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Assignment1/Calculator_Window/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_Window
+{
+    public partial class Form1 : Form
+    {
+        private TextBox activeTextBox; // 用于跟踪当前活动的文本框***
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 订阅文本框的Enter事件***
+            textBox1.Enter += TextBox_Enter;
+            textBox2.Enter += TextBox_Enter;
+        }
+
+        // 跟踪当前活动的文本框 ***
+        private void TextBox_Enter(object sender, EventArgs e)
+        {
+            activeTextBox = sender as TextBox;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e) //等号,计算并输出结果
+        {
+            try
+            {
+                double num1 = double.Parse(textBox1.Text);
+                double num2 = double.Parse(textBox2.Text);
+                double result = 0;
+
+                string op = comboBox1.SelectedItem.ToString();
+                switch (op)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        if (num2 != 0)
+                            result = num1 / num2;
+                        else
+                            MessageBox.Show("除数不能为零"); //弹窗除零报错
+                        break;
+                    default:
+                        MessageBox.Show("请选择运算符");
+                        break;
+                }
+                textBox3.Text = result.ToString();
+            }
+
+            catch(FormatException) 
+            {
+                MessageBox.Show("请输入有效的数字");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("发生错误: " + ex.Message);
+            }
+
+        }
+
+        private void button14_Click(object sender, EventArgs e) //小数点.
+        {
+            if (activeTextBox != null)
+            {
+                Button button = sender as Button;
+                activeTextBox.Text += ".";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) //1
+        {
+            if (activeTextBox != null)
+            {
+                Button button = sender as Button;
+                activeTextBox.Text += "1"; 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //2
+        {
+            if (activeTextBox != null)
+            {
+                Button button = sender as Button;
+                activeTextBox.Text += "2";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) //3
+        {
+            if (activeTextBox != null)
+            {
+                Button button = sender as Button;
+                activeTextBox.Text += "3";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) //4
+        {
+            if (activeTextBox != null)
+            {
+                Button button = sender as Button;
+                activeTextBox.Text += "4";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e) //5
+        {
+            if (activeTextBox != null)
+            {
+                Button button = sender as Button;
+                activeTextBox.Text += "5";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e) //6
+        {
+            if (activeTextBox != null)
+            {
+                Button button = sender as Button;
+                activeTextBox.Text += "6";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e) //7
+        {
+            if (activeTextBox != null)
+            {
+                Button button = sender as Button;
+                activeTextBox.Text += "7";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e) //8
+        {
+            if (activeTextBox != null)
+            {
+                Button button = sender as Button;
+                activeTextBox.Text += "8";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e) //9
+        {
+            if (activeTextBox != null)
+            {
+                Button button = sender as Button;
+                activeTextBox.Text += "9";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e) //Delete
+        {
+            if (activeTextBox != null)
+            {
+                Button button = sender as Button;
+                activeTextBox.Text = activeTextBox.Text.Substring(0, activeTextBox.Text.Length - 1);
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e) //清空计算器的输入CE键
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
