d55f27471b3b1840da11bea101246fc230f000ee
WhuerCSsheng <142279505+WhuerCSsheng@users.noreply.github.com>
2025-03-06 09:18:39 +0800

    Add files via upload

---
+++ assignment2/Program1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace primeDiv
+{
+    class solution1
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+            //输入部分
+                Console.WriteLine("\n请输入正整数:");
+                string sNum = Console.ReadLine();
+                bool con_flag = int.TryParse(sNum, out int num);//合法性输入检测
+
+                //Console.WriteLine(num);
+                //Console.WriteLine(con_flag);
+
+                if (!con_flag) //异常输入处理
+                {
+                    Console.WriteLine("非法输入!");
+                    continue;
+                }
+
+             //计算与输出部分
+                int record = 0, cycle = num;
+                for (int i = 2; (i * i) < cycle; i++)
+                {
+                    while (num % i == 0)
+                    {
+                        if (i != record)
+                        {
+                            Console.Write(i + " ");
+                            record = i;
+                        }
+                        num = num / i;
+                    }
+                }
+                if (num != record && num != 1)
+                    Console.Write(num);
+            }
+        }
+    }
+}
---
+++ assignment2/Program3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Buffers;
+
+namespace SOE //Sieve of Eratosthenes 埃筛
+{
+    class solution3
+    {
+        static void findPrimeWithin100()
+        {
+            bool[] arr = new bool[99];
+            for (int i = 0; i < 99; i++)
+            {
+                if (!arr[i])
+                {
+                    Console.Write((i + 2) + " ");
+                    int times = 1;//将所寻出的素数放大的倍率
+                    while (times * (i + 2) <= 100) //100以内，超出不考虑
+                    {
+                        arr[(i + 2) * times - 2] = true;//已排除的数（用数组下标指示）
+                        times++;
+                    }
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            findPrimeWithin100();
+        }
+    }
+
+}
